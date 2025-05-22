34d453f0f0e0b2f80e7e1a2c95c191bb78349ec6
LucaJackson <luca.jackson@stu.ocsb.ca>
2025-05-22 13:22:46 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("goToPage1");
+    button.addEventListener("click", () => {
+        window.location.href = "page1.html";
+    });
+});
