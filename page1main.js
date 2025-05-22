089130c663f15d2e7a89154c38e6d8bbdf9e41c1
LucaJackson <luca.jackson@stu.ocsb.ca>
2025-05-22 13:36:39 -0400

    Update page1main.js

--- page1main.js
+++ page1main.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("goToHomePage");
+    button.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+});
